14fa2dbc068a0c527ab737c927aef3034d677239
HamzaAhmadBhutta <f180368@cfd.nu.edu.pk>
2022-10-05 03:33:06 +0500

    Added bootstrap ,header and footer

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,18 @@
 import React from "react";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import "./bootstrap.min.css"
 
 const App=()=> {
   return (
      
      <>
-     
+     <Header/>
+     <main>
      <h1>HI hamza</h1>
-
+     </main>
+     <Footer/> 
+  
      </>
 
   );
