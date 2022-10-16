46cb86f275f8537035e382a56310400395aec224
HamzaAhmadBhutta <f180368@cfd.nu.edu.pk>
2022-10-17 00:01:42 +0500

    Product page is added

--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -11,10 +11,12 @@ import {
 } from "react-bootstrap";
 import Rating from "../components/Rating";
 import products from "../products";
+import { useParams } from "react-router-dom";
 
 const ProductScreen = ({ match }) => {
-  const product = products.find((p) => p._id === match.params.id);
+  const { id } = useParams();
 
+  const product = products.find((p) => p._id === id);
   return (
     <>
       <Link className="btn btn-dark my-3 " to="/">
