e2d960a811f84e63aa6a5a6bba5ac771394423b9
HamzaAhmadBhutta <f180368@cfd.nu.edu.pk>
2022-10-16 15:09:58 +0500

    s

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,25 +1,33 @@
-import Container from 'react-bootstrap/Container';
-import Nav from 'react-bootstrap/Nav';
-import Navbar from 'react-bootstrap/Navbar';
+import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+import { LinkContainer } from "react-router-bootstrap";
 
-
-const  ColorSchemesExample=()=> {
+const ColorSchemesExample = () => {
   return (
-    <> 
-      
-      <Navbar bg="light" variant="light" >
-         
+    <>
+      <Navbar bg="light" variant="light">
         <Container>
-          <Navbar.Brand href="/">Tech Bazaar</Navbar.Brand>
+          <LinkContainer to="/">
+            <Navbar.Brand>Tech Bazaar</Navbar.Brand>
+          </LinkContainer>
           <Nav className="ml -auto">
-            <Nav.Link href="/cart"><i className='fas fa-shopping-cart mx-1'></i>Cart</Nav.Link>
-            <Nav.Link href="/login"><i className='fas fa-user mx-1'></i>Sign In</Nav.Link>
+            <LinkContainer to="/cart">
+              <Nav.Link>
+                <i className="fas fa-shopping-cart mx-1"></i>Cart
+              </Nav.Link>
+            </LinkContainer>
+
+            <LinkContainer to="/login">
+              <Nav.Link>
+                <i className="fas fa-user mx-1"></i>Sign In
+              </Nav.Link>
+            </LinkContainer>
           </Nav>
         </Container>
       </Navbar>
     </>
-
   );
-}
+};
 
 export default ColorSchemesExample;
