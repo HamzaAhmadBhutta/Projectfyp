9a7cf9121a5e97fbe0ff0a11de239bd8e08ec334
HamzaAhmadBhutta <f180368@cfd.nu.edu.pk>
2022-10-16 14:27:34 +0500

    A

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,26 +1,29 @@
 import React from "react";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
-import "./bootstrap.min.css"
-import {Container} from 'react-bootstrap'
+import "./bootstrap.min.css";
+import { Container } from "react-bootstrap";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import HomeScreen from "./screens/HomeScreen.js";
+import ProductScreen from "./screens/ProductScreen.js";
 
-const App=()=> {
+const App = () => {
   return (
-     
-     <>
-     <Header/>
-     <main className="py-3">
-      <Container>
-
-     <HomeScreen/>
-      </Container>
-     </main>
-     <Footer/> 
-  
-     </>
-
+    <>
+      <Router>
+        <Header />
+        <main className="py-3">
+          <Container>
+            <Routes>
+              <Route path="/" element={<HomeScreen />} exact />
+              <Route path="/product/:id" element={<ProductScreen />} />
+            </Routes>
+          </Container>
+        </main>
+        <Footer />
+      </Router>
+    </>
   );
-}
+};
 
 export default App;
--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,7 +1,7 @@
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
-import {logo} from '../Assets/Img/logo.png'
+
 
 const  ColorSchemesExample=()=> {
   return (
