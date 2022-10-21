409a85d70c6711efe412bbb866a2ce6f16eb631a
HamzaAhmadBhutta <f180368@cfd.nu.edu.pk>
2022-10-21 21:54:14 +0500

    sa

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,7 @@ import express from "express";
 import dotenv from "dotenv";
 import connectDB from "./config/db.js";
 import color from "colors";
-
+import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
 import productRoutes from "./routes/productRoutes.js";
 
 dotenv.config();
@@ -16,6 +16,13 @@ app.get("/", (req, res) => {
 });
 
 app.use("/api/products", productRoutes);
+
+////
+/// not found error handle by middle war
+app.use(notFound);
+
+/// @middle ware conver req ,res html to json
+app.use(errorHandler);
 
 const PORT = process.env.PORT || 5000;
 
