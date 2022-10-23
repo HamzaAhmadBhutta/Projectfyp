592273baba38007e78c68f8bfceb0eb3b32f4608
HamzaAhmadBhutta <f180368@cfd.nu.edu.pk>
2022-10-23 16:36:19 +0500

    Login and sign up

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,9 +1,19 @@
-import Container from "react-bootstrap/Container";
-import Nav from "react-bootstrap/Nav";
-import Navbar from "react-bootstrap/Navbar";
+import React from "react";
+import { Route } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
 import { LinkContainer } from "react-router-bootstrap";
+import { Navbar, Nav, Container, NavDropdown } from "react-bootstrap";
+import SearchBox from "./SearchBox";
+import { logout } from "../actions/userActions";
 
 const ColorSchemesExample = () => {
+  const dispatch = useDispatch();
+
+  const userLogin = useSelector((state) => state.userLogin);
+  const { userInfo } = userLogin;
+  const logoutHandler = () => {
+    dispatch(logout());
+  };
   return (
     <>
       <Navbar bg="light" variant="light">
@@ -17,12 +27,22 @@ const ColorSchemesExample = () => {
                 <i className="fas fa-shopping-cart mx-1"></i>Cart
               </Nav.Link>
             </LinkContainer>
-
-            <LinkContainer to="/login">
-              <Nav.Link>
-                <i className="fas fa-user mx-1"></i>Sign In
-              </Nav.Link>
-            </LinkContainer>
+            {userInfo ? (
+              <NavDropdown title={userInfo.name} id="username">
+                <LinkContainer to="/profile">
+                  <NavDropdown.Item>Profile</NavDropdown.Item>
+                </LinkContainer>
+                <NavDropdown.Item onClick={logoutHandler}>
+                  Logout
+                </NavDropdown.Item>
+              </NavDropdown>
+            ) : (
+              <LinkContainer to="/login">
+                <Nav.Link>
+                  <i className="fas fa-user"></i> Sign In
+                </Nav.Link>
+              </LinkContainer>
+            )}
           </Nav>
         </Container>
       </Navbar>
